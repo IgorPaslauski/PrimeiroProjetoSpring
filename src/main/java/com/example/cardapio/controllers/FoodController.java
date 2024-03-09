2926b5b166c38e1ce7a0429f4193abd2f16c9aca
igorp <io006235@cfjl.com.br>
2024-03-08 21:30:22 -0300

    Implementado função de delete

--- src/main/java/com/example/cardapio/controllers/FoodController.java
+++ src/main/java/com/example/cardapio/controllers/FoodController.java
@@ -5,9 +5,12 @@ import com.example.cardapio.food.FoodRepository;
 import com.example.cardapio.food.FoodRequestDTO;
 import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("food")
@@ -19,6 +22,20 @@ public class FoodController {
     public void create(@RequestBody FoodRequestDTO data){
         Food foodData = new Food(data);
         repository.save(foodData);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> delete(@RequestBody long id){
+        Optional<Food> food = repository.findById(id);
+        ;
+        if (food.isEmpty())
+        {
+            return new ResponseEntity<>("id não localizado, registro não exlcuido!", HttpStatus.BAD_REQUEST);
+        }
+
+        repository.delete(food.get());
+
+        return new ResponseEntity<>("Registro removido com sucesso!", HttpStatus.OK);
     }
 
     @GetMapping
